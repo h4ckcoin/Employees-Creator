9addd072f7fa0fa3cb313bc7b98c81061f1d8e5f
cspro <cspro@G-TROJAN>
2022-12-07 21:08:45 +0300

    Add project files.

---
+++ Employees/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Employees
+{
+    internal class Program
+    {
+        static employee _calisan;
+        static void Main(string[] args)
+        {
+            CreateEmploy();
+            GetEmploye();
+        }
+        static void CreateEmploy()
+        {
+            // 1. Çalısan olusturulacak , isim soyisim , departman , maaş ,  inputu alınacak.    
+            // 2. class üzerinden inputlardan çalışan oluşturulacak.
+            // 3. 
+
+
+            Console.Write("Ad : ");
+            string Ad = Console.ReadLine();
+
+            Console.Write("Soyad : ");
+            string Soyad = Console.ReadLine();
+
+            Console.Write("Maas : ");
+            string Maas = Console.ReadLine();
+
+             _calisan = new employee()
+            {
+                ad = Ad,
+                soyad = Soyad,
+                maas = Maas,
+            };
+
+           
+        }
+        static void GetEmploye()
+        {
+            Console.WriteLine("*** Employe Creating ***");
+            Console.WriteLine($"Ad: {_calisan.ad}\n" +
+                $"Soyad : {_calisan.soyad}\n" +
+                $"Maas : {_calisan.maas}\n"
+                );
+
+        }
+
+
+
+    }
+}
---
+++ Employees/employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    internal class employee
+    {
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public string maas { get; set; }
+    }
+}
